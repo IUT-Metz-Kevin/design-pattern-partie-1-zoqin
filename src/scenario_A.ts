0b7db67953fcbf7be559764f6f22f5432b22f702
Paquet--Kremer Robin <robin.paquet-kremer3@etu.univ-lorraine.fr>
2025-04-23 09:49:18 +0200

    ajout méthode singleton paramètre gestion de paramètre en pourcentage

--- src/scenario_A.ts
+++ src/scenario_A.ts
@@ -1,7 +1,10 @@
 class Parametre {
-    private static _instance: Parametre
+    private static _instance: Parametre;
+    private _value: number;
 
-    private constructor() {}
+    private constructor(value = 50) {
+        this.value = value
+    }
 
     public static get instance() {
         if (this._instance == null)
@@ -10,7 +13,21 @@ class Parametre {
         }
         return this._instance;
     }
+
+    public set value(number: number) {
+        if(number<0 || number >100) {
+            throw new Error("la valeur doit être un pourcentage")
+        } else {
+            this._value = number;
+        }
+    }
+
+    public get value(): number {
+        return this._value;
+    }
 }
 
 let parametreAudio = Parametre.instance
 let parametreGraphic = Parametre.instance
+
+parametreAudio.value = 18
